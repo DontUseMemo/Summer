c64438397bf76977695bb571128cdfc07c1fe0cd
DontUseMemo <nann76554@gmail.com>
2022-08-08 01:34:13 +0900

    프로젝트 파일 올림

---
+++ personalPerfume/src/main/java/com/example/personalperfume/PersonalPerfumeApplication.java
@@ -0,0 +1,13 @@
+package com.example.personalperfume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalPerfumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalPerfumeApplication.class, args);
+    }
+
+}
---
+++ personalPerfume/src/test/java/com/example/personalperfume/PersonalPerfumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personalperfume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalPerfumeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
