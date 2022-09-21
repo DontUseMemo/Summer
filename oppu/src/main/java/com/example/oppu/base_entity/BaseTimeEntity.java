409cf6ab10b8b2e87de06b9a2fee8ba28deaf84a
DontUseMemo <nann76554@gmail.com>
2022-09-22 03:19:24 +0900

    자동으로 시간을 업데이트 해주는 베이스 엔터티 추가하고 메인클래스에 베이스 엔터티 사용하는 어노테이션 추가

---
+++ oppu/src/main/java/com/example/oppu/base_entity/BaseTimeEntity.java
@@ -0,0 +1,28 @@
+package com.example.oppu.base_entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+@Setter
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseTimeEntity {
+
+    @Temporal(TemporalType.DATE)
+    @CreatedDate
+    private Date createDate;
+
+    @Temporal(TemporalType.DATE)
+    @LastModifiedDate
+    private Date updateDate;
+}
