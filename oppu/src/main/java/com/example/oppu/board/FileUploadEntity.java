4fe975b334789aa6ba815ef86fb443e6729ce79c
ose720 <se68720@naver.com>
2022-10-12 15:05:39 +0900

    이미지 업로드 구현

---
+++ oppu/src/main/java/com/example/oppu/board/FileUploadEntity.java
@@ -0,0 +1,36 @@
+package com.example.oppu.board;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class FileUploadEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    private String uuid;
+
+    private String contentType;
+
+    private String name;
+
+    private String originalFileName;
+
+    private Long boardSeq;
+
+
+
+
+}
